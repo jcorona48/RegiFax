41320b3a87bffb28df0ea5c2c72198fb00c1cd99
WanderJ08 <wanderroque01@gmail.com>
2023-03-19 19:19:06 -0400

    procedimientos de Rol y Departamento agregados

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -87,38 +87,7 @@ go
 
 /* -------------------------------- PROCEDIMIENTOS DE CLIENTE ------------------------------- */
 
-CREATE PROC SP_REGISTRARCLIENTE(
 
-@Nombre varchar(50), --
-@Apellido varchar(50),--
-@Cedula varchar(13), --
-@Telefono varchar(12), --
-@Sexo varchar(50), --
-@Direccion varchar(250), --
-@Fecha_Nacimiento varchar(50),
-@Estado_Civil varchar(20),
-@Estado bit,
-@Resultado int output,
-@Mensaje varchar(500) output
-
-)
-
-as 
-
-begin
-	set @Resultado = 0
-	if not exists(select * from T_Cliente where Cedula = @Cedula)
-	begin 
-		insert into T_Cliente(Nombre, Apellido, Cedula, Telefono, Sexo, Direccion, Fecha_Nacimiento, Estado_Civil, Estado) values 
-		(@Nombre, @Apellido, @Cedula, @Telefono, @Sexo, @Direccion, @Fecha_Nacimiento, @Estado_Civil, @Estado)
-		set @Resultado = SCOPE_IDENTITY() 
-	end 
-	else
-		set @Mensaje = 'Ya existe un cliente con esta identificacion'
-end
-
-
-go
 
 
 CREATE PROC SP_EDITARCLIENTE(
@@ -168,4 +137,170 @@ end
 go
 
 /* -------------------------------- FIN PROCEDIMIENTOS CLIENTE --------------------------------*/
+
+/* -------------------------------- PROCEDIMIENTOS DE ROL ------------------------------- */
+go
+CREATE PROC SP_REGISTRARROL
+(
+	@Nombre varchar(50),
+	@Estado bit,
+	@Resultado int output,
+	@Mensaje varchar(500) output
+	)
+
+as 
+	begin
+		set @Resultado = 0
+		if not exists(select * from T_Rol where Nombre = @Nombre)
+		begin
+			insert into T_Rol(Nombre, Estado) values
+			(@Nombre, @Estado)
+
+			set @Resultado = SCOPE_IDENTITY() 
+		end
+		else 
+			set @Mensaje = 'Ya existe este rol'
+	end
+
+
+
+go
+CREATE PROC SP_EDITARROL
+(
+	@ID_Rol int,
+	@Nombre varchar(50),
+	@Estado bit,
+	@Resultado bit output,
+	@Mensaje varchar(500) output
+	)
+
+as 
+	begin
+		set @Resultado = 1
+		if not exists(select * from T_Rol where Nombre = @Nombre and ID_Rol != @ID_Rol)
+		begin
+			update T_Rol set
+			Nombre = @Nombre,
+			Estado = @Estado
+			where ID_Rol = @ID_Rol
+
+		end
+		else 
+			begin
+				set @Resultado = 0
+				set @Mensaje = 'Ya existe este rol'
+			end
+	end
+
+	go
+CREATE PROC SP_ELIMINARROL
+(
+	@ID_Rol int,
+	@Resultado bit output,
+	@Mensaje varchar(500) output
+	)
+
+as 
+	begin
+		set @Resultado = 1
+		if not exists(select * from T_Rol r 
+		inner join T_Usuario u on u.ID_Rol = r.ID_Rol
+		where r.ID_Rol = @ID_Rol)
+		begin
+			delete top (1) from T_Rol where ID_Rol = @ID_Rol
+		end
+		else 
+			begin
+				set @Resultado = 0
+				set @Mensaje = 'No se puede eliminar este rol, esta relacionada con un usuario'
+			end
+	end
+
+	/* -------------------------------- FIN PROCEDIMIENTOS ROL --------------------------------*/
+
+	/* ----------------------------------- PROCEDIMIENTOS DEPARTAMENTO -----------------------------------*/
+
+
+go
+CREATE PROC SP_REGISTRARDEPARTAMENTO
+(
+	@Nombre varchar(50),
+	@Descripcion varchar (250),
+	@Estado bit,
+	@Resultado int output,
+	@Mensaje varchar(500) output
+	)
+
+as 
+	begin
+		set @Resultado = 0
+		if not exists(select * from T_Departamento where Nombre = @Nombre)
+		begin
+			insert into T_Departamento(Nombre, Estado, Descripcion) values
+			(@Nombre, @Estado, @Descripcion)
+
+			set @Resultado = SCOPE_IDENTITY() 
+		end
+		else 
+			set @Mensaje = 'Ya existe este departamento'
+	end
+
+go
+CREATE PROC SP_EDITARDEPARTAMENTO
+(
+	@ID_Departamento int,
+	@Nombre varchar(50),
+	@Descripcion varchar(250),
+	@Estado bit,
+	@Resultado bit output,
+	@Mensaje varchar(500) output
+	)
+
+as 
+	begin
+		set @Resultado = 1
+		if not exists(select * from T_Departamento where Nombre = @Nombre and ID_Departamento != @ID_Departamento)
+		begin
+			update T_Departamento set
+			Nombre = @Nombre,
+			Descripcion = @Descripcion,
+			Estado = @Estado
+			where ID_Categoria = @ID_Categoria
+
+		end
+		else 
+			begin
+				set @Resultado = 0
+				set @Mensaje = 'Ya existe este departamento'
+			end
+	end
+
+
+
+go
+CREATE PROC SP_ELIMINARDEPARTAMENTO
+(
+	@ID_Departamento int,
+	@Resultado bit output,
+	@Mensaje varchar(500) output
+	)
+
+as 
+	begin
+		set @Resultado = 1
+		if not exists(select * from T_Departamento d 
+		inner join T_Empleado e on e.ID_Departamento = d.ID_Departamento
+		where d.ID_Departamento = @ID_Departamento)
+		begin
+			delete top (1) from T_Departamento where ID_Departamento = @ID_Departamento
+		end
+		else 
+			begin
+				set @Resultado = 0
+				set @Mensaje = 'No se puede eliminar este departamento, esta relacionada con un empleado'
+			end
+	end
+
+/* -------------------------------- FIN PROCEDIMIENTOS DEPARTAMENTO --------------------------------*/
+
 
