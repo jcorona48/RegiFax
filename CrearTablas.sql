8abf4279a05f56be7ed280e8bf0df299c5d53087
jcorona48 <105407871+jcorona48@users.noreply.github.com>
2023-03-19 14:40:43 -0400

    getdate

--- CrearTablas.sql
+++ CrearTablas.sql
@@ -82,7 +82,7 @@ create table T_Categoria(
 ID_Categoria int primary key IDENTITY,
 Nombre varchar(50),
 Estado binary default 1,
-Fecha_Registro datetime default NOW()
+Fecha_Registro datetime default GETDATE()
 );
 
 ----- Fin Esteban ------
